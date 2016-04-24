759699125ecff505a0de6968a5d2b241221aeaf7
Evgeniy Shurmin <eshurmin@gmail.com>
2016-04-24 15:30:02 +0300

    uuid test

--- src/uuid/uuid_test.go
+++ src/uuid/uuid_test.go
@@ -1,1 +1,15 @@
-package uuid
+package uuid_test
+
+import (
+	"testing"
+	"uuid"
+	"github.com/stretchr/testify/assert"
+	"regexp"
+	"fmt"
+)
+
+func TestNewUUID(t *testing.T) {
+	id := uuid.NewUUID()
+	n, _ := regexp.Match("[a-f0-9]{8}(?:-[a-f0-9]{4}){4}-[a-f0-9]{8}", []byte(id))
+	assert.Equal(t, n, true, fmt.Sprintf("'%v' is not UUID",id))
+}
